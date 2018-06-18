8beca96b478d66bba00c31b20a2c885da1bd2bbd
Noel <witherwings77@gmail.com>
2018-06-17 21:41:44 -0300

    Para Publications

--- desapp-groupc-backend/src/main/services/PublicationService.java
+++ desapp-groupc-backend/src/main/services/PublicationService.java
@@ -1,10 +1,24 @@
 package main.services;
 
+import main.model.Availability;
 import main.model.Publication;
 import main.model.Reservation;
+import main.model.builders.PublicationBuilder;
+import main.repositories.VehicleRepository;
+import org.joda.time.DateTime;
 
 public class PublicationService extends GenericService<Publication> {
   private static final long serialVersionUID = -2932116622242535843L;
+
+  public Publication createPublication(Integer vehicleId) {
+    Publication pub = new PublicationBuilder()
+      .withVehicle(VehicleRepository.findVehicleById(vehicleId))
+      .withOwner(VehicleRepository.findVehicleById(vehicleId).getOwner())
+      .withAvailability(new Availability(DateTime.now(),DateTime.now().plusDays(5)))
+      .build();
+    this.save(pub);
+    return pub;
+  }
 
   public void acceptReservation(Publication p, Reservation r) {
     p.acceptReservation(r);
